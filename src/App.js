b744c238b43302e727aea3865898ac32c5c4a3e5
Noreen Wu <noreenwu@gmail.com>
2019-06-24 17:29:46 +0000

    feat: passing in filtered array of currently read books to top BookShelf

--- src/App.js
+++ src/App.js
@@ -5,17 +5,74 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    localBooks: [
+      { title: 'To Kill a Mockingbird',
+      	authors: 'Harper Lee',
+        width: 128,
+        height: 188,
+        backgroundimg: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
+       shelf: 'currentlyReading'
+      },
+      { title: 'Ender\'s Game',
+        authors: 'Orson Scott Card',
+        width: 128, 
+        height: 188,
+        backgroundimg: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
+        shelf: 'currentlyReading'
+      },
+      { title: '1776',
+        authors: 'David McCullough',
+        width: 128, 
+        height: 193,
+        backgroundimg: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
+        shelf: 'wantToRead'
+      },
+      { title: 'Harry Potter and the Sorcerer\'s Stone',
+        authors: 'J.K. Rowling',
+        width: 128, 
+        height: 192,
+        backgroundimg: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
+        shelf: 'wantToRead',
+      },
+      {
+        title: 'The Hobbit',
+        authors: 'J.R.R. Tolkien',
+        width: 128,
+        height: 192,
+        backgroundimg: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
+        shelf: 'read'
+      },
+      {
+        title: 'Oh, the Places You\'ll Go!',
+        authors: 'Seuss',
+        width: 128,
+        height: 174,
+        backgroundimg: 'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
+        shelf: 'read'
+      },
+      {
+        title: 'The Adventures of Tom Sawyer',
+        authors: 'Mark Twain',
+        width: 128,
+        height: 192,
+        backgroundimg: 'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api',
+        shelf: 'read'
+      }      
+    ],
     books: [],
+    b1: {},
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
-     */
-    b1: '',
+     */    
     showSearchPage: false
   }
 
+  filterBooks = (b) => {
+    return (b.shelf === 'currentlyReading');
+  }
   componentDidMount() {
     BooksAPI.getAll()
     .then((books) => {
@@ -58,7 +115,8 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-				<BookShelf/>
+				<BookShelf shelfName={'Currently Reading'} 
+						   shelfBooks={this.state.localBooks.filter(this.filterBooks)}/>
           
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import Book from './Book'
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
-const BookShelf = () => {
+const BookShelf = (props) => {
 
   	return(
                 <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <h2 className="bookshelf-title">{props.shelfName}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
 						<Book/>
-                      </li>
-                      <li>
+{console.log(props.shelfBooks)}
+{props.shelfBooks.map((b) =>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                            <div className="book-cover book-cover-add" 
+								 style={{ width: 128, height: 188, backgroundImage: b.backgroundimg }}></div>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -26,14 +27,20 @@ const BookShelf = () => {
                               </select>
                             </div>
                           </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
+                          <div className="book-title">{b.title}</div>
+                          <div className="book-authors">{b.author}</div>
                         </div>
+)}
                       </li>
+
                     </ol>
                   </div>
                 </div>
 	)
 }
 
+BookShelf.propTypes = {
+  shelfName: PropTypes.string.isRequired,
+  shelfBooks: PropTypes.array.isRequired
+};
 export default BookShelf
